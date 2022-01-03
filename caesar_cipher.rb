a514026008e271082be9c6706dc0eb76d10e877a
Ricardo <ricardomg92@hotmail.com>
2022-01-03 21:58:50 +0100

    added methods for user input and text encoding

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,14 @@
-puts "What string you want to code?"
-uncoded_string = gets
-puts uncoded_string
-puts "For how much do you want to shift it?"
-shift_factor = gets
-puts shift_factor
+def user_input
+  puts "What string you want to code?"
+  uncoded_string = gets
+  puts "For how much do you want to shift it?"
+  shift_factor = gets
+  encoding(uncoded_string, shift_factor)
+end
+
+def encoding(uncoded_string, shift_factor)
+  puts uncoded_string
+  puts shift_factor
+end
+
+user_input
