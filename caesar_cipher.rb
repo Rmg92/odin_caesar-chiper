945375978a0ba37d5430009ea6772ba1dd71054e
Ricardo <ricardomg92@hotmail.com>
2022-01-04 22:09:46 +0100

    New line is removed from the inputs, shift factor gets converted to Integer, each letter of the uncoded_string gets converted to an array

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,13 +1,18 @@
+require 'pry-byebug'
+
 def user_input
   puts "What string you want to code?"
-  uncoded_string = gets
+  unecrypted_string = gets.chomp
   puts "For how much do you want to shift it?"
-  shift_factor = gets
-  encoding(uncoded_string, shift_factor)
+  shift_factor = gets.chomp.to_i
+  encrypt(unecrypted_string, shift_factor)
 end
 
-def encoding(uncoded_string, shift_factor)
-  puts uncoded_string
+def encrypt(unecrypted_string, shift_factor)
+  binding.pry
+  unecrypted_array = unecrypted_string.chars
+  puts unecrypted_string
+  puts unecrypted_array
   puts shift_factor
 end
 
