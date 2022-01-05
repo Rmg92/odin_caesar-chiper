5735ad7cacaabb44fc9a38c4a641972565380f70
Ricardo <ricardomg92@hotmail.com>
2022-01-05 21:44:34 +0100

    improved variable names

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,26 +1,26 @@
-require "pry-byebug"
+#require "pry-byebug"
 
 def user_input
   puts "What string you want to code?"
-  unecrypted_string = gets.chomp
+  unecrypted_message = gets.chomp
   puts "For how much do you want to shift it?"
   shift_factor = gets.chomp.to_i
-  encrypt(unecrypted_string, shift_factor)
+  encrypt(unecrypted_message, shift_factor)
 end
 
-def encrypt(unecrypted_string, shift_factor)
-  binding.pry
+def encrypt(unecrypted_message, shift_factor)
+  #binding.pry
   # converts the passed unecrypted_string to an array
-  unecrypted_array = unecrypted_string.chars
+  unecrypted_array = unecrypted_message.chars
   # converts the each character of the unecrypted_array to ordinal
   ordinal_array = unecrypted_array.map { |letter| letter.ord }
   # adds the shift factor to each ordinal on the ordinal_array
   encoded_ordinal_array = ordinal_array.map { |number| number + shift_factor }
   # converts the encoded_ordinal_array ordinals back to letters
-  encoded_array = encoded_ordinal_array.map { |number| number.chr}
+  encoded_array = encoded_ordinal_array.map { |number| number.chr }
   # converts the encoded_array back to a string
-  encoded_string = encoded_array.join
-  puts encoded_string
+  encoded_message = encoded_array.join
+  puts encoded_message
 end
 
 user_input
