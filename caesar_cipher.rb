1f2fc96e76a6dc7722f0b86354d46798b1143a97
Ricardo <ricardomg92@hotmail.com>
2022-01-06 22:06:28 +0100

    correctly wraps back to A when using big positive shift factors

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -16,12 +16,20 @@ def encrypt(unecrypted_message, shift_factor)
   ordinal_array = unecrypted_array.map { |letter| letter.ord }
   # adds the shift factor to each ordinal on the ordinal_array
   encoded_ordinal_array = ordinal_array.map do |number|
-    if number < 65 || (number.between?(91, 96)) || number > 122
+    if number < 65 || number.between?(91, 96) || number > 122
       number
-    elsif number.between?(65, 90) && number + shift_factor > 90 
-      number + shift_factor - 26
+    elsif number.between?(65, 90) && number + shift_factor > 90
+      new_shift_factor = shift_factor
+      while number + new_shift_factor > 90
+        new_shift_factor -= 26
+      end
+      number + new_shift_factor
     elsif number.between?(97, 122) && number + shift_factor > 122
-      number + shift_factor - 26
+      new_shift_factor = shift_factor
+      while number + new_shift_factor > 122
+        new_shift_factor -= 26
+      end
+      number + new_shift_factor
     else
       number + shift_factor
     end
